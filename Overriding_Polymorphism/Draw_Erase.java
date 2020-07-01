19089d04c8b1fed2a8a816cc3c9485e96358daba
udaykumarsamala <58910376+udaykumarsamala@users.noreply.github.com>
2020-07-01 22:02:02 +0530

    Update Draw_Erase.java

--- Overriding_Polymorphism/Draw_Erase.java
+++ Overriding_Polymorphism/Draw_Erase.java
@@ -1,61 +1,61 @@
 package Overriding_Polymorphism;
 
-public class Draw_Erase {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Shape c = new Circle();
-		Shape t = new Triangle();
-		Shape s = new Square();
-		
-		c.draw();
-		c.erase();
-		
-		t.draw();
-		t.erase();
-		
-		s.draw();
-		s.erase();
-
-	}
-
-}
-class Shape {
-	
-	
-	public void draw() {
+class Shape
+{
+	void draw()
+	{
 		System.out.println("Drawing Shape");
 	}
-	
-	public void erase() {
+	void erase()
+	{
 		System.out.println("Erasing Shape");
 	}
-	
 }
-class Circle extends Shape {
-
-	public void draw() {
+class Circle extends Shape
+{
+	void draw()
+	{
 		System.out.println("Drawing Circle");
 	}
-	public void erase() {
+	void erase()
+	{
 		System.out.println("Erasing Circle");
 	}
 }
-class Triangle extends Shape {
-
-	public void draw() {
+class Triangle extends Shape
+{
+	void draw()
+	{
 		System.out.println("Drawing Triangle");
 	}
-	public void erase() {
+	void erase()
+	{
 		System.out.println("Erasing Triangle");
 	}
 }
-class Square extends Shape {
-
-	public void draw() {
-		System.out.println("Drawing Square");
+class Square extends Shape
+{
+	void draw()
+	{
+		System.out.println("Drawing Shape");
 	}
-	public void erase() {
-		System.out.println("Erasing Square");
+	void erase()
+	{
+		System.out.println("Erasing Shape");
 	}
+}
+public class Draw_Erase
+{
+	public static void main(String args[])
+	{
+		Shape c = new Circle();
+		Shape t = new Triangle();
+		Shape s = new Square();
+		c.draw();
+		c.erase();
+		t.draw();
+		t.erase();
+		s.draw();
+		s.erase();
+	}	
 }
